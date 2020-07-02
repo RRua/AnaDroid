b7b6559263437768112209c10185baeddb5294d5
Rui Rua <rui.rrua@gmail.com>
2020-07-02 15:52:18 +0100

    isolate succ v

---
+++ isolateSuccessVersions.sh
@@ -0,0 +1,29 @@
+# !/bin/bash
+
+default_location="/home/greenlab/Anadroid//demoProjects"
+#default_location="./samples"
+
+target_success_apps_dir="$HOME/successAppsFolder"
+
+#for version in $(cat suc.log); do 
+for version in $(cat $ANADROID_PATH/.ana/logs/success.log); do 
+	folder_version=$(grep "$version" $ANADROID_PATH/.ana/logs/processedApps.log )
+	target_app_dir=$( dirname $folder_version | xargs dirname | xargs basename  )
+	version_app_dir=$( dirname $folder_version  | xargs basename  )
+
+	echo "$version  -   $target_app_dir --  $version_app_dir"
+	target_dir="$target_success_apps_dir/$target_app_dir/$version_app_dir"
+	echo "creating $target_dir"
+	#mkdir -p "$target_dir"
+	echo "cp -r $folder_version $target_dir"
+	exit 0 
+	#echo "cp -r $folder_version $target_success_apps_dir"
+	#exit 0
+	#version_pack=$(find $default_location -mindepth 3 -maxdepth 4 -name $version -type f )
+	#echo "folder - $version_pack"
+	#app_name=$( echo "$version" | cut -f9 -d\/ ) #"sed 's#'"${default_location}"'##g' )
+	#echo "->$app_name"
+	#mkdir -p "outDir/$app_name"
+	#cp $version_pack "outDir/$app_name"
+
+done
